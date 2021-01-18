fcfaa0666f26465d47e5574d73caa82c1ea0149b
jacob <Jacoblinden@hotmail.com>
2021-01-18 16:05:24 +0100

    ok

--- src/GUI.java
+++ src/GUI.java
@@ -3,7 +3,7 @@ public class GUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("setting up, 1");
+		System.out.print("setting up, 1,2");
 	}
 
 }
